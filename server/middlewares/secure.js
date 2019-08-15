0fcabd92854f17c9c0509fd5b87e10f5a9f5d403
diegomendezp <diegomen15@gmail.com>
2019-08-15 10:55:27 +0200

    Secure middleware created

---
+++ server/middlewares/secure.js
@@ -0,0 +1,9 @@
+const createError = require('http-errors');
+
+module.exports.isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    throw createError(403);
+  }
+};
