978b4012165705e416f2e77aba1e2b81730275c6
diegomendezp <diegomen15@gmail.com>
2019-09-10 18:29:37 +0200

    Display users skills chart

---
+++ client/src/components/RadarChart.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Radar } from "react-chartjs-2";
+
+const getData = statisticsAverage => {
+  return {
+    labels: ["Drive", "Backhand", "Serve", "Volley", "Resistance"],
+    datasets: [
+      {
+        label: "Personal Statistics",
+        data: [
+          statisticsAverage.drive.length == 0
+            ? 5
+            : statisticsAverage.drive.reduce((a, b) => a + b) /
+              statisticsAverage.drive.length,
+          statisticsAverage.backhand.length == 0
+            ? 5
+            : statisticsAverage.backhand.reduce((a, b) => a + b) /
+              statisticsAverage.backhand.length,
+          statisticsAverage.serve.length == 0
+            ? 5
+            : statisticsAverage.serve.reduce((a, b) => a + b) /
+              statisticsAverage.serve.length,
+          statisticsAverage.volley.length == 0
+            ? 5
+            : statisticsAverage.volley.reduce((a, b) => a + b) /
+              statisticsAverage.volley.length,
+          statisticsAverage.resistance.length == 0
+            ? 5
+            : statisticsAverage.resistance.reduce((a, b) => a + b) /
+              statisticsAverage.resistance.length
+        ],
+        borderColor: "rgba(20, 29, 222, 1)",
+        backgroundColor: "rgba(20, 29, 222, 0.2)"
+      },
+      {
+        label: ["  Media"],
+        data: [1, 2, 5, 8, 4],
+        borderColor: "rgba(255, 99, 132, 0.2)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)"
+      }
+    ],
+    options: {
+      position: "left",
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        display: true,
+        position: "bottom",
+        labels: {}
+      },
+      scale: {
+        ticks: {
+          display: false,
+          max: 10,
+          min: 0
+        }
+      }
+    }
+  };
+};
+
+const options = {
+  position: "bottom",
+  responsive: true,
+  maintainAspectRatio: false,
+  legend: {
+    display: true,
+    position: "bottom",
+    labels: {}
+  }
+};
+export default function RadarChart({ statisticsAverage }) {
+  return (
+    <div className="chart-container">
+      <Radar data={getData(statisticsAverage)} legend={options} />
+    </div>
+  );
+}
--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -4,12 +4,13 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
-
+import { Radar } from "react-chartjs-2";
+import RadarChart from "./RadarChart";
 
 const useStyles = makeStyles(theme => ({
   card: {
     display: "flex",
-    flexDirection:"column",
+    flexDirection: "column",
     [theme.breakpoints.down("sm")]: {
       width: "90%",
       margin: "0 auto 5% auto"
@@ -45,40 +46,36 @@ const useStyles = makeStyles(theme => ({
     height: 150,
     [theme.breakpoints.down("sm")]: {
       width: 80,
-      height: 80,
+      height: 80
     },
     [theme.breakpoints.up("sm")]: {
       width: 100,
-      height: 100,
+      height: 100
     },
     [theme.breakpoints.up("md")]: {
       width: 120,
-    height: 120,
+      height: 120
     },
     margin: "0 auto",
     marginBottom: "5%"
-  },
+  }
 }));
 
-
-export default function UserCard ({ image, username}) {
+export default function UserCard({ image, username, statisticsAverage }) {
   const classes = useStyles();
 
   return (
     <Card className={classes.card}>
       <div className={classes.details}>
         <CardContent className={classes.content}>
-          <Avatar
-            alt="username-icon"
-            src={image}
-            className={classes.avatar}
-          />
+          <Avatar alt="username-icon" src={image} className={classes.avatar} />
           <Typography component="h5" variant="h5">
             {username}
           </Typography>
           <Typography variant="subtitle1" color="textSecondary">
             Usuario
           </Typography>
+          <RadarChart statisticsAverage={statisticsAverage}></RadarChart>
         </CardContent>
       </div>
     </Card>
