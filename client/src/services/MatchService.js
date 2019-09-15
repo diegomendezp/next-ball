640450ad238498185a4ac09f8698606f1d346fdd
diegomendezp <diegomen15@gmail.com>
2019-09-15 10:58:17 +0200

    Ser winner & set loser match method added to Match Service

--- client/src/services/MatchService.js
+++ client/src/services/MatchService.js
@@ -13,40 +13,67 @@ class MatchService {
   }
 
   static getMatches = () => {
-    return instance.get("/").then(response => response.data)
-    .catch(MatchService.errorHandler)
+    return instance
+      .get("/")
+      .then(response => response.data)
+      .catch(MatchService.errorHandler);
   };
 
-  static newMatch = (match) => {
-    return instance.post("/new", match).then(response => response.data)
-    .catch(MatchService.errorHandler)
+  static newMatch = match => {
+    return instance
+      .post("/new", match)
+      .then(response => response.data)
+      .catch(MatchService.errorHandler);
   };
 
-  static getRecord = (id) => {
-    return instance.get(`/record/${id}`).then(response => response.data)
-    .catch(MatchService.errorHandler)
-  } 
+  static getRecord = id => {
+    return instance
+      .get(`/record/${id}`)
+      .then(response => response.data)
+      .catch(MatchService.errorHandler);
+  };
 
   static getMyMatches = () => {
-    return instance.get(`/my-matches`).then(response => response.data)
-    .catch(MatchService.errorHandler)
-  } 
-
-  static addPlayer = (playerId, matchId) => {
-    return instance.post(`/addPlayer/${playerId}/${matchId}`, {}).then(response => response.data)
-    .catch(MatchService.errorHandler)
-  }
-
-  static deleteMatch = (id) => {
-    return instance.delete(`/${id}`).then(response => response.data)
-    .catch(MatchService.errorHandler)
-  } 
-
-  static finishMatch = (match, winner, loser) => {
-    return instance.post(`/endMatch/${match}`, {winner, loser}).then(response => response.data)
-    .catch(MatchService.errorHandler)
+    return instance
+      .get(`/my-matches`)
+      .then(response => response.data)
+      .catch(MatchService.errorHandler);
   };
 
+  static addPlayer = (playerId, matchId) => {
+    return instance
+      .post(`/addPlayer/${playerId}/${matchId}`, {})
+      .then(response => response.data)
+      .catch(MatchService.errorHandler);
+  };
+
+  static deleteMatch = id => {
+    return instance
+      .delete(`/${id}`)
+      .then(response => response.data)
+      .catch(MatchService.errorHandler);
+  };
+
+  static finishMatch = (match, winner, loser) => {
+    return instance
+      .post(`/endMatch/${match}`, { winner, loser })
+      .then(response => response.data)
+      .catch(MatchService.errorHandler);
+  };
+
+  static setWinner(id) {
+    return instance
+      .post(`matches/winnerEndMatch`, { winner: id })
+      .then(response => response.data)
+      .catch(MatchService.errorHandler);
+  }
+
+  static setLoser(id) {
+    return instance
+      .post(`matches/loserEndMatch`, { loser: id })
+      .then(response => response.data)
+      .catch(MatchService.errorHandler);
+  }
 }
 
 export default MatchService;
